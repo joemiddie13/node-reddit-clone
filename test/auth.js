5298331fdec93c3f2aad056051a997384ee08db3
joemiddie13 <joemiddie13@gmail.com>
2024-04-15 21:22:49 -0700

    implemented authentication tests

---
+++ test/auth.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { describe, it, before, after } = require('mocha');
+const app = require('../server');
+const User = require('../models/user');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+// Agent that will keep track of our cookies
+const agent = chai.request.agent(app);
+
+describe('User', function () {
+  it('should be able to signup', function (done) {
+    User.findOneAndRemove({ username: 'testone' }, function() {
+      agent
+        .post('/sign-up')
+        .send({ username: 'testone', password: 'password' })
+        .end(function (err, res) {
+          console.log(res.body);
+          res.should.have.status(200);
+          agent.should.have.cookie('nToken');
+          done();
+        });
+    });
+  });
+
+  it('should be able to login', function (done) {
+    agent
+      .post('/login')
+      .send({ username: 'testone', password: 'password' })
+      .end(function (err, res) {
+        res.should.have.status(200);
+        agent.should.have.cookie('nToken');
+        done();
+      });
+  });
+
+  it('should be able to logout', function (done) {
+    agent.get('/logout').end(function (err, res) {
+      res.should.have.status(200);
+      agent.should.not.have.cookie('nToken');
+      done();
+    });
+  });
+  after(function () {
+    agent.close();
+  });
+});
--- test/posts.js
+++ test/posts.js
@@ -1,15 +1,30 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { describe, it, after } = require('mocha');
+const { describe, it, before, after } = require('mocha');
 chai.use(chaiHttp);
 const app = require('../server');
 const agent = chai.request.agent(app);
-
-// Import the Post model from our models folder so we
-// we can use it in our tests.
 const Post = require('../models/post');
-
+const User = require('../models/user');
 const should = chai.should();
+
+const user = {
+  username: 'poststest',
+  password: 'testposts',
+};
+
+before(function (done) {
+  agent
+    .post('/sign-up')
+    .set('content-type', 'application/x-www-form-urlencoded')
+    .send(user)
+    .then(function (res) {
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+});
 
 describe('Posts', () => {
   // Post that we'll use for testing purposes
@@ -18,7 +33,8 @@ describe('Posts', () => {
     url: 'https://www.google.com',
     summary: 'post summary'
   };
-    it('Should create with valid attributes at POST /posts/new', (done) => {
+
+  it('Should create with valid attributes at POST /posts/new', (done) => {
     // Checks how many posts there are now
     Post.estimatedDocumentCount()
       .then((initialDocCount) => {
@@ -35,7 +51,7 @@ describe('Posts', () => {
                 // Check that the database has status 200
                 res.should.have.status(200);
                 // Check that the database has one more post in it
-                newDocCount.should.equal(initialDocCount + 1)
+                newDocCount.should.equal(initialDocCount + 1);
                 done();
               })
               .catch((err) => {
@@ -47,10 +63,24 @@ describe('Posts', () => {
           });
       })
       .catch((err) => {
-          done(err);
+        done(err);
       });
   });
-  after(() => {
-    Post.findOneAndDelete(newPost);
+
+  after(function (done) {
+    Post.findOneAndDelete(newPost)
+      .then(function () {
+        agent.close();
+        User.findOneAndDelete({ username: user.username })
+          .then(function () {
+            done();
+          })
+          .catch(function (err) {
+            done(err);
+          });
+      })
+      .catch(function (err) {
+        done(err);
+      });
   });
 });
